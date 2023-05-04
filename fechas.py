9ab02cd4ee827bddef22eda1c9c6d902c8aca482
Pablo <pablogcg05@gmail.com>
2023-05-04 16:14:42 +0200

    Creación código para una fecha

---
+++ fechas.py
@@ -0,0 +1,9 @@
+print("Hola, vamos a aprender fechas")
+paz_de_westfalia = "La Paz de Westfalia"
+fecha_paz_de_westfalia = int(1648)
+print("En qué año se produjo", paz_de_westfalia)
+entrada_paz_de_westfalia = int(input())
+if fecha_paz_de_westfalia == entrada_paz_de_westfalia:
+    print("¡Has acertado!")
+else:
+    print("Has fallado, fue en", fecha_paz_de_westfalia)
