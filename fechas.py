669792bb5063737e9879f61666f044a7fbe64920
Pablo <pablogcg05@gmail.com>
2023-05-05 19:08:44 +0200

    Preguntas fuera del juego

--- fechas.py
+++ fechas.py
@@ -1,15 +1,5 @@
 import random
-
-preguntas = [
-    {
-        "pregunta": "¿En qué año se firmó la paz de Westfalia?",
-        "respuesta": "1648"
-    },
-    {
-        "pregunta": "¿En qué año finalizó el reinado de Carlos II?",
-        "respuesta": "1700"
-    }
-]
+from preguntas_fechas import preguntas
 
 
 def jugar():
---
+++ preguntas_fechas.py
@@ -0,0 +1,10 @@
+preguntas = [
+    {
+        "pregunta": "¿En qué año se firmó la paz de Westfalia?",
+        "respuesta": "1648"
+    },
+    {
+        "pregunta": "¿En qué año finalizó el reinado de Carlos II?",
+        "respuesta": "1700"
+    }
+]
