c8b8e52411d8b0b63ff82bbeb4ca06da0f49229e
Pablo <pablogcg05@gmail.com>
2023-05-05 20:39:04 +0200

    Adición de todas las fechas

--- preguntas_fechas.py
+++ preguntas_fechas.py
@@ -88,7 +88,83 @@ preguntas = [
         "respuesta": "1716"
     },
     {
-        "pregunta": "",
-        "respuesta": ""
+        "pregunta": "¿En que año se firma el primer pacto de familia?",
+        "respuesta": "1733"
     },
+    {
+        "pregunta": "¿En qué año se promulgó el catastro del Marqués de Ensenada?",
+        "respuesta": "1749"
+    },
+    {
+        "pregunta": "¿En qué año se produjo el motín de Esquilache?",
+        "respuesta": "1766"
+    },
+    {
+        "pregunta": "¿En qué año se expulsó a los jesuitas?",
+        "respuesta": "1767"
+    },
+    {
+        "pregunta": "¿En que año se invistió a Godoy como primer ministro?",
+        "respuesta": "1792"
+    },
+    {
+        "pregunta": "¿En qué año se produjo la Batalla de Trafalgar?",
+        "respuesta": "1805"
+    },
+    {
+        "pregunta": "¿En qué año se produjo el motín de Aranjuez?",
+        "respuesta": "1808"
+    },
+    {
+        "pregunta": "¿En qué año se produjo el pronunciamiento de Riego?",
+        "respuesta": "1820"
+    },
+    {
+        "pregunta": "¿En qué año se fundó el partido demócrata?",
+        "respuesta": "1849"
+    },
+    {
+        "pregunta": "¿En qué año se produjo la huelga general de Barcelona?",
+        "respuesta": "1855"
+    },
+    {
+        "pregunta": "¿En qué año se promulgó la Ley de Instrucción Pública de Claudio Moyano?",
+        "respuesta": "1857"
+    },
+    {
+        "pregunta": "¿En qué año se proclamó la Primera República?",
+        "respuesta": "1873"
+    },
+    {
+        "pregunta": "¿En qué año se fundó el PSOE?",
+        "respuesta": "1879"
+    },
+    {
+        "pregunta": "¿En qué año se produjo la Semana Trágica de Barcelona?",
+        "respuesta": "1909"
+    },
+    {
+        "pregunta": "¿En qué año se promulga la Ley de Bases de la Reforma Agrária?",
+        "respuesta": "1932"
+    },
+    {
+        "pregunta": "¿En qué año se formó el comité de no intervención?",
+        "respuesta": "1936"
+    },
+    {
+        "pregunta": "¿En qué año se promulgó el Plan de Estabilización?",
+        "respuesta": "1959"
+    },
+    {
+        "pregunta": "¿En qué año se produjo el Referéndum para la Ley de Reforma Política?",
+        "respuesta": "1976"
+    },
+    {
+        "pregunta": "¿En qué año se produjo el 23F?",
+        "respuesta": "1981"
+    },
+    {
+        "pregunta": "¿En qué año entró España en la CEE?",
+        "respuesta": "1986"
+    }
 ]
