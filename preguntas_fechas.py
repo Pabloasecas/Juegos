b1abeab3a28a4648e42f40a3ac1ae3f6e5a7f32f
Pablo <pablogcg05@gmail.com>
2023-05-05 20:17:44 +0200

    Adición mitad de las fechas

--- preguntas_fechas.py
+++ preguntas_fechas.py
@@ -6,5 +6,89 @@ preguntas = [
     {
         "pregunta": "¿En qué año finalizó el reinado de Carlos II?",
         "respuesta": "1700"
-    }
+    },
+    {
+        "pregunta": "¿En qué año se produjo la conversión forzosa de los mudéjares?",
+        "respuesta": "1502"
+    },
+    {
+        "pregunta": "¿En qué año se conquistó el reino de Navarra?",
+        "respuesta": "1512"
+    },
+    {
+        "pregunta": "¿En qué año se produjo la revuela de los comuneros?",
+        "respuesta": "1520"
+    },
+    {
+        "pregunta": "¿En qué año comenzó el reinado de Felipe II?",
+        "respuesta": "1556"
+    },
+    {
+        "pregunta": "¿En qué año se estableció la corte en Madrid?",
+        "respuesta": "1561"
+    },
+    {
+        "pregunta": "¿En qué año se produjo la rebelión morisca de las Alpujarras?",
+        "respuesta": "1568"
+    },
+    {
+        "pregunta": "¿En qué año se produjo la batalla de Lepanto?",
+        "respuesta": "1571"
+    },
+    {
+        "pregunta": "¿En qué año se anexionó Portugal?",
+        "respuesta": "1580"
+    },
+    {
+        "pregunta": "¿En qué año se produjo el desastre de la Grande y Felicísima Armada?",
+        "respuesta": "1586"
+    },
+    {
+        "pregunta": "¿En qué año se expulsó a los moriscos?",
+        "respuesta": "1609"
+    },
+    {
+        "pregunta": "¿En qué año subió al trono Felipe IV?",
+        "respuesta": "1621"
+    },
+    {
+        "pregunta": "¿En qué año se produjo la rebelión de Cataluña?",
+        "respuesta": "1640"
+    },
+    {
+        "pregunta": "¿En qué año se independizó Portugal?",
+        "respuesta": "1640"
+    },
+    {
+        "pregunta": "¿En qué año se produjo la batalla de Rocroi?",
+        "respuesta": "1643"
+    },
+    {
+        "pregunta": "¿En qué año se firmó la paz de los Pirinéos?",
+        "respuesta": "1659"
+    },
+    {
+        "pregunta": "En qué año comienza el reinado de Carlos II",
+        "respuesta": "1665"
+    },
+    {
+        "pregunta": "¿En qué año inició la Guerra de Sucesión?",
+        "respuesta": "1701"
+    },
+    {
+        "pregunta": "¿En qué año se produce la conquista británica de Gibraltar?",
+        "respuesta": "1704"
+    },
+    {
+        "pregunta": "¿En qué año se firmó el tratado de Utrecht?",
+        "respuesta": "1713"
+    },
+    {
+        "pregunta": "¿En qué año se promulgan los Decretos de Nueva Planta de Cataluña?",
+        "respuesta": "1716"
+    },
+    {
+        "pregunta": "",
+        "respuesta": ""
+    },
 ]
